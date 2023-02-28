3192204cf871bd8b18abfbfb2fc8c4d86e2bb107
Felipe Pavanela <felipe-pavanela@hotmail.com>
2023-02-28 22:00:22 +1300

    Add projects thumbnails

--- app/components/Home/Content/ProjectsFile.tsx
+++ app/components/Home/Content/ProjectsFile.tsx
@@ -1,8 +1,17 @@
-import { Box, Button, Stack } from "@chakra-ui/react";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { Box, Button, Flex, Image, Stack } from "@chakra-ui/react";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Tag } from "~/components/CodeElements/Tag";
 import { ReactFile } from "../../CodeElements/ReactFile";
+
+import CommuteThumbnail from "~/assets/images/thumbnails/commutevisualiser_web_thumbnail.png";
+import DartsThumbnail from "~/assets/images/thumbnails/darts_app_thumbnail.png";
+import FlagFinderThumbnail from "~/assets/images/thumbnails/flagfinder_app_thumbnail.png";
+
+const imageProps = {
+    height: 200,
+    width: 200,
+};
 
 const ProjectsFile = () => {
     return (
@@ -13,17 +22,41 @@ const ProjectsFile = () => {
                     name="Flex"
                     tagProps={[{ key: "direction", value: "row" }]}
                 >
-                    <Stack spacing={2} my={2} direction="row">
-                        <Box width={150} height={150} backgroundColor="white" />
-                        <Box width={150} height={150} backgroundColor="white" />
-                        <Box width={150} height={150} backgroundColor="white" />
-                    </Stack>
+                    <Flex my={1} direction="row" flexWrap="wrap">
+                        <Box as="a" href="/projects/darts" m={2}>
+                            <Image
+                                borderRadius="lg"
+                                {...imageProps}
+                                src={DartsThumbnail}
+                            />
+                        </Box>
+                        <Box as="a" href="/projects/darts" m={2}>
+                            <Image
+                                borderRadius="lg"
+                                {...imageProps}
+                                src={CommuteThumbnail}
+                            />
+                        </Box>
+                        <Box as="a" href="/projects/darts" m={2}>
+                            <Image
+                                borderRadius="lg"
+                                {...imageProps}
+                                src={FlagFinderThumbnail}
+                            />
+                        </Box>
+                    </Flex>
                 </Tag>
                 <Tag addSpacingToChildren name="Button">
-                    <Button colorScheme="blue" my={2} size="sm">
+                    <Button
+                        as="a"
+                        colorScheme="blue"
+                        my={2}
+                        size="sm"
+                        href="/projects"
+                    >
                         VIEW ALL
                         <Box as="span" ml={2}>
-                            <FontAwesomeIcon icon={faArrowRight} />
+                            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                         </Box>
                     </Button>
                 </Tag>
