a16ccb8c04b8555da429812e8cdc11e5c42d7747
Felipe Pavanela <felipe-pavanela@hotmail.com>
2023-04-01 12:56:56 +1300

    Add link to repo

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -12,54 +12,56 @@ const year = new Date().getFullYear();
 const Footer = () => {
     const { isMobile } = useWebsiteBreakpoints();
 
+    if (isMobile) {
+        return (
+            <footer>
+                <MobileFooter />
+            </footer>
+        );
+    }
+
     return (
         <footer>
-            {isMobile ? (
-                <MobileFooter />
-            ) : (
-                <Flex
-                    position="relative"
-                    justify="space-between"
-                    width="100%"
-                    height={FOOTER_HEIGHT}
+            <Flex
+                position="relative"
+                justify="space-between"
+                width="100%"
+                height={FOOTER_HEIGHT}
+                alignItems="center"
+                backgroundColor="footerBackgroundColor"
+            >
+                <Stack
+                    spacing={4}
+                    direction="row"
+                    color="white"
                     alignItems="center"
-                    backgroundColor="footerBackgroundColor"
                 >
-                    <Stack
-                        spacing={4}
-                        direction="row"
-                        color="white"
-                        alignItems="center"
-                    >
-                        <Flex px={2} alignItems="center">
-                            {/* TODO: uncomment once README if finished */}
-                            {/* <Flex
+                    <Flex
                         as="a"
                         px={2}
                         alignItems="center"
                         target="_blank"
                         href="https://github.com/felipe-mapa/portfolio-v2"
-                    > */}
-                            <FontAwesomeIcon icon={faCodeBranch} />
-                            <Text ps={2}>main</Text>
-                        </Flex>
-                    </Stack>
-                    <Text textAlign="end" px={2} color="white" fontSize={14}>
-                        © Felipe Pavanela {year}
-                    </Text>
-                    <Tooltip
-                        label="Oh no! You found a bug on my website!"
-                        placement="top-start"
                     >
-                        <Box pe={3}>
-                            <FontAwesomeIcon
-                                icon={faBug}
-                                color={theme.colors.gray[900]}
-                            />
-                        </Box>
-                    </Tooltip>
-                </Flex>
-            )}
+                        <FontAwesomeIcon icon={faCodeBranch} />
+                        <Text ps={2}>main</Text>
+                    </Flex>
+                </Stack>
+                <Text textAlign="end" px={2} color="white" fontSize={14}>
+                    © Felipe Pavanela {year}
+                </Text>
+                <Tooltip
+                    label="Oh no! You found a bug on my website!"
+                    placement="top-start"
+                >
+                    <Box pe={3}>
+                        <FontAwesomeIcon
+                            icon={faBug}
+                            color={theme.colors.gray[900]}
+                        />
+                    </Box>
+                </Tooltip>
+            </Flex>
         </footer>
     );
 };
